83fcd038f52ee25e17dcfe5f46e694bb976ae495
Ken Seah <seahken@gmail.com>
2024-02-12 16:48:29 +1100

    added and refractor required functions

--- index.js
+++ index.js
@@ -1,47 +1,89 @@
-var fs = require('fs');
-
-var path = process.cwd();
-var buffer = fs.readFileSync(path + "/programming-task-example-data.log");
-
-const splittedString = buffer.toString().split('\n')
+const fs = require('fs');
 
 // parse log 
-const parsedObjArray = splittedString.map((string => {
-    const splitObj = string.split(' ')
-    return {
-        ipAddress: splitObj[0],
-        resource: splitObj[6],
-    }
-})).filter(obj => obj.ipAddress)
+function parseLogIntoArrayFromFile(filename) {
+    const path = process.cwd();
+    const buffer = fs.readFileSync(path + filename);
+    const splittedString = buffer.toString().split('\n')
 
-// find number of unique IP addresses
-function getNumberOfUniqueProp(arr, propName) {
-    var counts = {};
-    for (var i = 0; i < arr.length; i++) {
-        counts[arr[i][propName]] = 1 + (counts[arr[i][propName]] || 0);
-    }
+    const parsedObjArray = splittedString.map((string => {
+        const splitObj = string.split(' ')
+        return {
+            ipAddress: splitObj[0],
+            resource: splitObj[6],
+        }
+    })).filter(obj => obj.ipAddress)
 
-    return counts
+    return parsedObjArray
 }
 
+function getArrayNumberOfUniqueProp(arr, propName) {
 
-    // top 3 most visted URL
-    // top 3 IP addresses
+    const countObj = {};
+    for (let i = 0; i < arr.length; i++) {
+        countObj[arr[i][propName]] = 1 + (countObj[arr[i][propName]] || 0);
+    }
+
+    // convert object into array
+    const array = []
+    for (const [key, value] of Object.entries(countObj)) {
+        array.push({
+            value: key,
+            count: value
+        })
+    }
+    return array
+}
+
+// sort array according to count
+function sortAccordingToCount(array) {
+    return array.sort((a, b) => b.count - a.count)
+}
+
+// return top three values, accourding to count
+function returnTopThreeAccordingToCount(array) {
+    const countLimit = 3
+    const returnArray = []
+    let currentCount = undefined
+    const valueArray = []
+
+    for (let i = 0; i < array.length; i++) {
+        currentCount = array[i].count        
+        
+        if (currentCount !== array[i + 1]?.count) {
+            valueArray.push(array[i].value)
+            returnArray.push({
+                count: currentCount,
+                value: [...valueArray]
+            })
+            valueArray.splice(0,valueArray.length)
+        } else {
+            valueArray.push(array[i].value)
+        }
+    }
+
+    return returnArray.slice(0, countLimit)
+}
+
+function getNumberOfUnique(filename, propName) {
+    const array = parseLogIntoArrayFromFile(filename)   
+    return getArrayNumberOfUniqueProp(array, propName).length
+}
+
+function getTop3CountValues(filename, propName) {
+    const array = parseLogIntoArrayFromFile(filename)   
+    const threeMostVisitedUrls = sortAccordingToCount(getArrayNumberOfUniqueProp(array, propName))
+    return returnTopThreeAccordingToCount(threeMostVisitedUrls)
+}
+
+// find number of unique IP addresses
+console.log('getNumberOfUniqueIpAddress', getNumberOfUnique('/programming-task-example-data.log', 'ipAddress'))
+
+// top 3 most visted URL
+console.log('getTop3CountValues resource', getTop3CountValues('/programming-task-example-data.log', 'resource'))
+
+// // top 3 IP addresses
+console.log('getTop3CountValues ipAddress', getTop3CountValues('/programming-task-example-data.log', 'ipAddress'))
 
 
-console.log(parsedObjArray)
-
-const ipAddressObj = getNumberOfUniqueProp(parsedObjArray, 'resource') 
-
-const array = []
-for (const [key, value] of Object.entries(ipAddressObj)) {
-    array.push({
-        value: key,
-        count: value
-    })
-  }
-
-  console.log(array)
-
-
-
+module.exports = { getNumberOfUnique, getTop3CountValues }
